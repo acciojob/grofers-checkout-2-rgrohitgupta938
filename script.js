c999c2e03506cf277668c2ee29accee3301e735f
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-04-26 03:56:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+document.addEventListener("DOMContentLoaded", () => {
+  function calculateTotal() {
+    const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
+
+    let total = 0;
+
+    priceElements.forEach((priceElement) => {
+      const price = parseFloat(priceElement.textContent);
+      total += price;
+    });
+
+    return total.toFixed(2);
+  }
+
+  function updateTotal() {
+    const grandTotalElement = document.querySelector(
+      '[data-ns-test="grandTotal"]'
+    );
+    const total = calculateTotal();
+    grandTotalElement.textContent = total;
+  }
+
+  updateTotal();
+});
